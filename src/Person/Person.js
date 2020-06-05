4fc55ebb000d3015ad0af7a0258c6f00eeacd445
Sunil Jain <sjain@benelog.com>
2020-06-05 17:57:44 +0530

    special React property: children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,17 @@ const person = (props) => {
         are stored as one JSON object. We can name it whatever we want. Standard way is to name it props.
         Then you can get individual element from this props JSON object and get values.    
     */}
-    return <p>I am {props.name} and I am {props.age} years old</p>
+    {/*
+       We can access the data/content of any component using the special attribute 'children'.
+       This will give you access to any data/content that is passed to this custom HTML element/component at runtime.
+       We can access it pretty much the same way you would access any other attributes. 
+    */}
+    return (
+        <div>
+            <p>I am {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+    );
 }
 
 export default person;
