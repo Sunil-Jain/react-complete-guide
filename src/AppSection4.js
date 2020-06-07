ad55ba699e22523322655e5262f27e6965d22f04
Sunil Jain <sjain@benelog.com>
2020-06-07 11:45:58 +0530

    Load HTML content conditionally based on ternary codition

---
+++ src/AppSection4.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person.js'
+
+class App extends Component {
+
+  state = {
+    persons: [
+      { name:"Vinodbhai", age:47 },
+      { name:"Sanjuben", age:43 },
+      { name:"Sunil", age:25 },
+      { name:"Kunal", age:21 }
+    ],
+    showPersons: false
+  }
+
+  togglePersonsHandler = () => {
+    const currentState = this.state.showPersons;
+    this.setState({
+        showPersons: !currentState
+    });
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid black',
+      padding: '8px'
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <button 
+          style={style}
+          onClick={this.togglePersonsHandler}>Show persons</button>
+        {
+            this.state.showPersons ? 
+                <div>
+                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                    <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+                </div> : null
+        }
+      </div>
+      
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './AppSection4';
 //import App from './AppWithHooks'
 import registerServiceWorker from './registerServiceWorker';
 
