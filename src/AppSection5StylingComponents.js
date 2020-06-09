bc8b38fd556c5b99dd87e491cbbba31e4c132914
Sunil Jain <sjain@benelog.com>
2020-06-09 10:13:40 +0530

    Working with CSS modules

--- src/AppSection5StylingComponents.js
+++ src/AppSection5StylingComponents.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person.js';
 
 class App extends Component {
@@ -53,6 +53,9 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClass = [classes.button];
+
+
 
     if ( this.state.showPersons ) {
         persons = (
@@ -68,28 +71,23 @@ class App extends Component {
                 }
             </div> 
         );
-
-        // style.backgroundColor = 'red';
-        // style[':hover'] = {
-        //     backgroundColor: 'lightred',
-        //     color: 'black'
-        // }
+        btnClass.push(classes.Red);
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if (this.state.persons.length <= 2) {
-        classes.push('red'); // classes = ['red']
+        assignedClasses.push(classes.red); // classes = ['red']
     }
     if (this.state.persons.length <= 1) {
-        classes.push('bold'); // classes = ['red', 'bold']
+        assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
             <h1>Hi, I am a React App</h1>
-            <p className={classes.join(' ')}>This is really working</p>
-            <button className="button" onClick={this.togglePersonsHandler}>Toggle persons</button>
+            <p className={assignedClasses.join(' ')}>This is really working</p>
+            <button className={btnClass.join(' ')} onClick={this.togglePersonsHandler}>Toggle persons</button>
             {persons}
         </div>
     );
